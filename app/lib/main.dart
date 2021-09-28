70afdeda17b9cad9fc9c232d46eb216a811b61fd
JohnMaalouf1 <john.maalouf1@marist.edu>
2021-09-27 23:54:59 -0400

    Added New User Page
    Added a new dart file/page to allow a new user to input their information

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,115 +1,215 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(new MyApp());
+
+String username='';
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter App with MYSQL',     
+      home: new MyHomePage(),
+     
     );
   }
 }
 
+
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+    bool validPassword = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+
+TextEditingController name=new TextEditingController();
+TextEditingController email=new TextEditingController();
+TextEditingController password=new TextEditingController();
+TextEditingController dob=new TextEditingController();
+TextEditingController height=new TextEditingController();
+TextEditingController weight=new TextEditingController();
+
+
+void post() async {
+	print("Made it to method");
+	  final response = await http.post("http://192.168.0.174/MealTimeServer/config.php", body: {
+	    "name": name.text,
+	    "email": email.text,
+	    "password":password.text,
+		"dob":dob.text,
+    "height":height.text,
+    "weight":weight.text,
+
+	  });
+	  print("completed Method");
+	}
+
+
+ void validateTextField(String userInput) {
+   
+
+   if (userInput.length >= 8 && (userInput.contains(new RegExp(r'[!@#$%^&*(),.?":{}|<>]')))){
+     setState(() {
+         validPassword = false;
+       });
+   } else  {
+     setState(() {
+         validPassword = true;
+       });
+   }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: AppBar(title: Text("Create Account"),
+      actions: <Widget>[
+    IconButton(
+      icon: Icon(
+        Icons.account_circle_outlined,
+        color: Colors.white,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      onPressed: () {
+        // do something
+      },
+    )
+  ],
+),
+
+      
+      
+
+      body: SingleChildScrollView(
+        padding: new EdgeInsets.only(
+          top: 20.0,
+          right: 40.0,
+          left: 40.0,
+        ),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+			      Image.asset('assets/images/newuser.png'),
+
+
+
+              TextField(   
+                controller: name,          
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  hintText: 'Your Username:',                  
+                  hintStyle: TextStyle(color: Colors.blue),
+                  contentPadding: EdgeInsets.all(10),
+
+                  constraints: BoxConstraints.tightFor(width: 350),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),                SizedBox(height: 8.0),
+
+               
+              TextField(   
+                controller: email,          
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'example@mail.com:',
+                  hintStyle: TextStyle(color: Colors.blue),
+                  contentPadding: EdgeInsets.all(10),
+                  constraints: BoxConstraints.tightFor(width: 350),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),                SizedBox(height: 8.0),
+
+
+
+              TextField(   
+                controller: password,       
+                                  obscureText: true,
+   
+                  decoration: InputDecoration(
+                  errorText: validPassword ? 'Please enter a valid password' : null,
+                  labelText: 'Password',
+                  hintText: '8 characters long with Symbol',
+                  hintStyle: TextStyle(color: Colors.blue),
+                  contentPadding: EdgeInsets.all(10),
+                  constraints: BoxConstraints.tightFor(width: 350),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),                SizedBox(height: 8.0),          
+
+
+              TextField(   
+                controller: dob,          
+                decoration: InputDecoration(
+                  labelText: 'Date Of Birth: ',
+                  hintText: 'dd/mm/yyyy:',
+                  hintStyle: TextStyle(color: Colors.blue),
+                  contentPadding: EdgeInsets.all(10),
+                  constraints: BoxConstraints.tightFor(width: 350),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),                SizedBox(height: 8.0),
+
+
+              TextField(   
+                controller: height,          
+                decoration: InputDecoration(
+                  labelText: 'Height',
+                  hintText: "Ex: 5'8:",
+                  hintStyle: TextStyle(color: Colors.blue),
+                  contentPadding: EdgeInsets.all(10),
+                  constraints: BoxConstraints.tightFor(width: 350),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),                SizedBox(height: 8.0),
+
+              TextField(   
+                controller: weight,          
+                decoration: InputDecoration(
+                  labelText: 'Weight',
+                  hintText: 'Ex: 160: ',
+                  hintStyle: TextStyle(color: Colors.blue),
+                  contentPadding: EdgeInsets.all(10),
+                  constraints: BoxConstraints.tightFor(width: 350),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),                SizedBox(height: 8.0),
+
+              MaterialButton(
+                minWidth: 200.0,
+                height: 40.0,
+                child: Text("Register"),
+                color: Colors.blue,
+                elevation: 5,
+                
+
+                onPressed: (){
+                  validateTextField(password.text);                  
+                  post();
+                },
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
+}
 }
