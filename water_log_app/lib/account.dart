9d1de9a672c2af37b6f1d46c8baa4457c70a408b
JohnMaalouf1 <john.maalouf1@marist.edu>
2021-10-13 13:55:27 -0400

    Added Graph Page

--- water_log_app/lib/account.dart
+++ water_log_app/lib/account.dart
@@ -1,138 +1,90 @@
 import 'package:flutter/material.dart';
-import 'package:water_log_app/custom_theme.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:intl/intl.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
+class account extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: theme_class.light_theme,
-      home: account(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class account extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
   @override
-  account_state createState() => account_state();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class account_state extends State<account> {
+class _MyHomePageState extends State<MyHomePage> {
+  late List<GDPData> _chartData;
+  late TooltipBehavior _tooltipBehavior;
+
+  @override
+  void initState() {
+    _chartData = getChartData();
+    _tooltipBehavior = TooltipBehavior(enable: true);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text("Account"),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 60.0),
-              child: FittedBox(
-                child: Container(
-                  width: 200,
-                  height: 150,
-                  alignment: Alignment.topRight,
-                  /*decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(50.0)),*/
-                  // TODO ADD IMAGE HERE child: Image.asset('ADD ASSET HERE')
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Text(
-                "Name: Bob Roberts",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Text(
-                "Weight: 300lbs",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Text(
-                "Height: 5'11",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Text(
-                "BMI: 26.6",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Text(
-                "Date Of Birth: 05/23/2006",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Text(
-                "Gender: Male",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Text(
-                "Email: Bobert.roberts@hotmail.com",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Text(
-                "Username: BobertTheRobert",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Text(
-                "Weight: 300lbs",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 130,
-            ),
+    return SafeArea(
+        child: Scaffold(
+      body: SfCartesianChart(
+          title: ChartTitle(text: 'Weekly Water Usage'),
+          legend: Legend(isVisible: true),
+          tooltipBehavior: _tooltipBehavior,
+          series: <ChartSeries>[
+            BarSeries<GDPData, String>(
+                isTrackVisible: false,
+                opacity: 0.5,
+                color: const Color.fromRGBO(51, 153, 255, 255),
+                name: 'Water Usage',
+                dataSource: _chartData,
+                pointColorMapper: (GDPData sales, _) => sales.segmentColor,
+                xValueMapper: (GDPData gdp, _) => gdp.continent,
+                yValueMapper: (GDPData gdp, _) => gdp.gdp,
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+                dataLabelSettings: DataLabelSettings(isVisible: true),
+                width: 0.8, // Width of the bars
+                spacing: 0.2, // Spacing between the bars
+                enableTooltip: true)
           ],
-        ),
-      ),
-    );
+          primaryXAxis: CategoryAxis(),
+          primaryYAxis: NumericAxis(
+              edgeLabelPlacement: EdgeLabelPlacement.shift,
+              //numberFormat: NumberFormat.simpleCurrency(decimalDigits: 0)
+              title: AxisTitle(text: 'Gallons Per Day - (09/27/2021)'))),
+    ));
   }
+
+  List<GDPData> getChartData() {
+    final List<GDPData> chartData = [
+      GDPData('Sunday', 16, Colors.blue),
+      GDPData('Saturday', 46, Colors.red),
+      GDPData('Friday', 15, Colors.blue),
+      GDPData('Thursday', 42, Colors.red),
+      GDPData('Wednesday', 30, Colors.green),
+      GDPData('Tuesday', 25, Colors.green),
+      GDPData('Monday', 46, Colors.red),
+    ];
+    return chartData;
+  }
+}
+
+class GDPData {
+  GDPData(this.continent, this.gdp, this.segmentColor);
+  final String continent;
+  final double gdp;
+  final Color segmentColor;
 }
