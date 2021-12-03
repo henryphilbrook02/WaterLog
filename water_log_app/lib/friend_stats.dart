235aae48eb6c0a581cdd3c14ef2412bfe5955671
JohnMaalouf1 <john.maalouf1@marist.edu>
2021-12-03 15:47:13 -0500

    Implemented Stats Feature into friends stats page

--- water_log_app/lib/friend_stats.dart
+++ water_log_app/lib/friend_stats.dart
@@ -3,6 +3,20 @@ import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:intl/intl.dart';
 import 'package:water_log_app/models/userModel.dart' as userModel;
 
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:intl/intl.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:water_log_app/models/userModel.dart' as userModel;
+import 'package:water_log_app/custom_theme.dart';
+
+var average = 0.0;
+var totalNums = 2;
+final now = new DateTime.now();
+String formatter = DateFormat('yMd').format(now); // 28/03/2020
+
 class statsFriends extends StatelessWidget {
   // This widget is the root of your application.
 
@@ -26,14 +40,12 @@ class statsFriends extends StatelessWidget {
 }
 
 class MyHomePageFriends extends StatefulWidget {
-
   String username;
 
   MyHomePageFriends({
     Key? key,
     required this.username,
   }) : super(key: key);
-
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -44,10 +56,63 @@ class _MyHomePageState extends State<MyHomePageFriends> {
   late TooltipBehavior _tooltipBehavior;
   bool showPassword = false;
 
+  var sum = 0.0;
+  var day = "";
+  var myDay;
+  var mySums = [];
+  var myDays = [];
+  var testAverage = 0;
+
+  //  Need to use the passed username here
+  Future<wData> postRequest() async {
+    final response = await http.get(Uri.parse(
+        'http://10.11.25.60:443/api/seven_day_readout/' + widget.username));
+    average = 0;
+    if (response.statusCode == 200) {
+      for (var i = 0; i < 7; i++) {
+        try {
+          var mainUser = wData.fromJson(jsonDecode(response.body)[i]);
+          sum = mainUser.wSum.toDouble();
+          day = mainUser.wDay.toString();
+          DateTime dt = DateTime.parse(day);
+          myDay = DateFormat('EEEE').format(dt);
+          average = average + sum;
+          print("Average: " + average.toString());
+          mySums.add(sum);
+          myDays.add(myDay);
+          testAverage++;
+        } on Exception catch (exception) {
+          print("Error");
+        } catch (error) {
+          print("Error");
+        }
+      }
+
+      return wData.fromJson(jsonDecode(response.body)[0]);
+    } else {
+      throw Exception('Failed to load user');
+    }
+  }
+
   @override
   void initState() {
-    _chartData = getChartData();
     _tooltipBehavior = TooltipBehavior(enable: true);
+    _chartData = getChartData();
+
+    Future.delayed(Duration.zero, () async {
+      await postRequest();
+
+      setState(() {
+        mySums = mySums;
+      });
+      setState(() {
+        myDays = myDays;
+      });
+
+      _tooltipBehavior = TooltipBehavior(enable: true);
+      _chartData = getChartData();
+    });
+
     super.initState();
   }
 
@@ -189,9 +254,12 @@ class _MyHomePageState extends State<MyHomePageFriends> {
 
   @override
   Widget buildGraph(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
+    print(formatter);
+    // _chartData = getChartData();
+    // _tooltipBehavior = TooltipBehavior(enable: true);
+    return Scaffold(
       body: SfCartesianChart(
+          title: ChartTitle(text: 'Weekly Water Usage'),
           legend: Legend(isVisible: true),
           tooltipBehavior: _tooltipBehavior,
           series: <ChartSeries>[
@@ -199,7 +267,7 @@ class _MyHomePageState extends State<MyHomePageFriends> {
                 isTrackVisible: false,
                 opacity: 0.5,
                 color: const Color.fromRGBO(51, 153, 255, 255),
-                name: 'Water Usage',
+                name: "Average: " + (average / mySums.length).toString(),
                 dataSource: _chartData,
                 pointColorMapper: (GDPData sales, _) => sales.segmentColor,
                 xValueMapper: (GDPData gdp, _) => gdp.continent,
@@ -214,20 +282,35 @@ class _MyHomePageState extends State<MyHomePageFriends> {
           primaryYAxis: NumericAxis(
               edgeLabelPlacement: EdgeLabelPlacement.shift,
               //numberFormat: NumberFormat.simpleCurrency(decimalDigits: 0)
-              title: AxisTitle(text: 'Gallons Per Day - (09/27/2021)'))),
-    ));
+              title: AxisTitle(text: 'Gallons Per Day - ' + formatter))),
+    );
   }
 
   List<GDPData> getChartData() {
-    final List<GDPData> chartData = [
-      GDPData('Sunday', 16, Colors.blue),
-      GDPData('Saturday', 46, Colors.red),
-      GDPData('Friday', 15, Colors.blue),
-      GDPData('Thursday', 42, Colors.red),
-      GDPData('Wednesday', 30, Colors.green),
-      GDPData('Tuesday', 25, Colors.green),
-      GDPData('Monday', 46, Colors.red),
-    ];
+    final List<GDPData> chartData = [];
+
+    try {
+      for (int i = 0; i < mySums.length; i++) {
+        var fullAverage = average / mySums.length;
+        var lowerAverage = fullAverage - 10;
+        var highAverage = fullAverage + 10;
+        print("Full: " + fullAverage.toString());
+        var gColor;
+        if ((mySums[i] > highAverage)) {
+          gColor = Colors.red;
+        } else if (mySums[i] < lowerAverage) {
+          gColor = Colors.green;
+        } else {
+          gColor = Colors.blue;
+        }
+        chartData.add(GDPData(myDays[i], mySums[i], gColor));
+      }
+    } on Exception catch (exception) {
+      print("Error"); // only executed if error is of type Exception
+    } catch (error) {
+      print("Error"); // only executed if error is of type Exception
+      // executed for errors of all types other than Exception
+    }
     return chartData;
   }
 }
@@ -239,3 +322,13 @@ class GDPData {
   final Color segmentColor;
 }
 
+class wData {
+  int wSum;
+  String wDay;
+
+  wData({required this.wSum, required this.wDay});
+
+  factory wData.fromJson(Map<String, dynamic> json) {
+    return wData(wSum: json['sum(amount)'], wDay: json['day']);
+  }
+}
